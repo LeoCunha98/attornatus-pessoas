f2532eed6cc2b48cd22a697752e41eea2afd8229
Leonardo Cunha <leonardo.cunhajf@gmail.com>
2023-10-11 16:05:38 -0300

    Chore: criação da aplicação e packages iniciais

---
+++ src/test/java/com/tecnico/attornatus/pessoas/PessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnico.attornatus.pessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
